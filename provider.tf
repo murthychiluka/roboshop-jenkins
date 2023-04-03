edeee83a918dfb353c49a7ce404484e2d4a908e2
Murthy <murthychiluka@gmail.com>
2023-04-03 21:12:47 +0530

    jenkins

--- provider.tf
+++ provider.tf
@@ -1,7 +1,16 @@
 # Configure the Jenkins Provider
 provider "jenkins" {
-  server_url = "https://172.31.8.9:8080" # Or use JENKINS_URL env var
-  username   = "username"                # Or use JENKINS_USERNAME env var
-  password   = "password"                # Or use JENKINS_PASSWORD env var
-  ca_cert    = ""                        # Or use JENKINS_CA_CERT env var
+  server_url = "https://172.31.8.9:8080"
+  username   = "aws_ssm_parameter.jenkins_user.value"
+  password   = "aws_ssm_parameter.jenkins_pass.value"
+
 }
+
+data "aws_ssm_parameter" "jenkins_user" {
+  name = "jenkins.user"
+}
+
+data "aws_ssm_parameter" "jenkins_pass" {
+  name = "jenkins.pass"
+}
+
