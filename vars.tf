85a0142e1ae596bfdd440f9b3f17df374a041c35
Murthy <murthychiluka@gmail.com>
2023-04-04 17:53:37 +0530

    jenkins

--- vars.tf
+++ vars.tf
@@ -6,12 +6,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "infra", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/frontend" }
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/cart" }
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/catalogue" }
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/user" }
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/shipping" }
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/payment" }
 
 
 
