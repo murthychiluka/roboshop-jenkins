2f8c211d7c9a506f0caff54fc6bc33b7fdbe28a0
Murthy <murthychiluka@gmail.com>
2023-04-04 17:47:53 +0530

    jenkins

--- vars.tf
+++ vars.tf
@@ -1,10 +1,21 @@
 variable "folders" {
-  default = ["infra"]
+  default = ["infra", "CI-Pipeline"]
 
 }
 
 variable "jobs" {
   default = [
     { name = "roboshop", folder = "infra", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/roboshop.infra" }
+
+
+
+
+
   ]
 }
