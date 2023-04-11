b99afe8c6fa5733917ece412fd053ef04b641701
murthychiluka <murthychiluka@gmail.com>
2023-04-11 21:33:52 +0530

    mb

--- vars.tf
+++ vars.tf
@@ -4,16 +4,7 @@ variable "folders" {
 }
 
 variable "s-jobs" {
-  default = [
-    { name = "roboshop", folder = "infra", repo_url = "https://github.com/murthychiluka/roboshop.infra" },
-    { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/frontend" },
-    { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/cart" },
-    { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/catalogue" },
-    { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/user" },
-    { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/shipping" },
-    { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/murthychiluka/payment" }
-
-  ]
+  default = [ ]
 }
 
 variable "m-jobs" {
